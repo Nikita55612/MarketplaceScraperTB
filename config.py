5e90511954c681176be1331333fd93a086aba60f
Nikita <116632007+Nikita55612@users.noreply.github.com>
2025-01-31 19:31:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
-#test 5784668759:AAEebBmBdCUXUX_MyjU5mMpsJp8LQx7tchQ
-#test2 6734347719:AAEEP-kUqd_r_JlNoy0nyz0bPaTvKtoBDPg
-#main 6827193677:AAFQ1I8MYPET9hkfvPCxP--Ww3aGLJ1mgzQ
+#test 57846875:AAEebBmBd...
+#test2 6734319:AAEEP-k...
+#main 6819677:AAFQ1I8...
 
 #----------------------------------------------------------------
-TELEBOT_TOKEN = "6734347719:AAEEP-kUqd_r_JlNoy0nyz0bPaTvKtoBDPg"
+TELEBOT_TOKEN = "..."
 #----------------------------------------------------------------
 
 DIR = os.path.dirname(os.path.abspath(__file__))
@@ -31,7 +31,7 @@ SCRAPER_LOOP_TIMEOUT = 3500
 MAX_TABLE_REQ = 300
 MAX_STREAM_PRODUCTS = 10000
 AUTHORIZATION_KEYS_PATH = "bot/authorization_keys.json"
-GPT_API_KEY = "sk-ece8f0db3ddd472ab05e045fdc3c92f2"
+GPT_API_KEY = "sk-ece8f0db3ddd472af2"
 GPT_BASE_URL = "https://api.deepseek.com/v1"
 GPT_MODEL = "deepseek-chat"
 DOC_URL = r"https://sites.google.com/view/marketplacelivescraper/documentation"
